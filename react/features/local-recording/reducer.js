1a060968b30fb1bb6ae3303e76090b463b07157b
Radium Zheng <ztl8702@msn.com>
2018-06-06 06:55:57 +1000

    local-recording(encoding): flac support with libflac.js

--- react/features/local-recording/reducer.js
+++ react/features/local-recording/reducer.js
@@ -3,9 +3,11 @@ import { ReducerRegistry } from '../base/redux';
 import {
     LOCAL_RECORDING_TOGGLE
 } from './actionTypes';
+import { RecordingController } from './RecordingController';
 
 
 ReducerRegistry.register('features/local-recording', (state = {}, action) => {
+    console.log(state);
     switch (action.type) {
     case LOCAL_RECORDING_TOGGLE: {
         const oldOnOffState = state.on;
@@ -27,8 +29,7 @@ ReducerRegistry.register('features/local-recording', (state = {}, action) => {
     }
 });
 
-// let myAudioStream = null;
-let mediaRecorder = null;
+const recordingController = new RecordingController();
 
 /**
  * Starts MediaRecorder.
@@ -36,30 +37,7 @@ let mediaRecorder = null;
  * @returns {void}
  */
 function startRecording() {
-    if (mediaRecorder === null) {
-        navigator.getUserMedia(
-
-            // constraints - only audio needed for this app
-            {
-                audioBitsPerSecond: 256000, // 256 kbps
-                audio: true,
-                mimeType: 'application/ogg'
-            },
-
-            // Success callback
-            stream => {
-                // myAudioStream = stream;
-                mediaRecorder = new MediaRecorder(stream);
-                mediaRecorder.ondataavailable = e => saveMediaData(e.data);
-            },
-
-            // Error callback
-            err => {
-                console.log(`The following gUM error occurred: ${err}`);
-            }
-        );
-    }
-    mediaRecorder.start();
+    recordingController.startRecording();
 }
 
 /**
@@ -68,26 +46,7 @@ function startRecording() {
  * @returns {undefined}
  */
 function stopRecording() {
-    mediaRecorder.stop();
+    recordingController.stopRecording().then(
+        () => recordingController.downloadRecordedData());
 }
 
-/**
- * Saves audio data to a file.
- *
- * @param {*} data - Audio data from MediaRecorder.
- * @returns {undefined}
- */
-function saveMediaData(data) {
-    const audioURL = window.URL.createObjectURL(data);
-
-    console.log('Audio URL:', audioURL);
-
-    // auto save audio
-    const a = document.createElement('a');
-
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    a.href = audioURL;
-    a.download = 'recording.ogg';
-    a.click();
-}
